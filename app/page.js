8c79cd7bb17cc7773a7f33d87bc1459620b8a374
Dhanush Kodi <dhanushvijayakumar@gmail.com>
2023-03-18 15:04:46 +0530

    added additional background

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className="bg-gradient-to-r from-amber-500 via-blue-500 to-fuchsia-500 overflow-hidden">
+  <div className="bg-gradient-to-r from-amber-500 via-blue-500 via-green-500 to-fuchsia-500 overflow-hidden">
     <Navbar />
     <Hero />
     <div className="relative">
