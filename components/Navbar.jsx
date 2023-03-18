015827ba000c8c8106548a71a4f349d39072a0f1
Dhanush Kodi <dhanushvijayakumar@gmail.com>
2023-03-18 10:19:04 +0530

    added correct css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import styles from '../styles.module.css';
+import styles from '../styles.globals.css';
 import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
